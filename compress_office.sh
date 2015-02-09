a2b91cbb918909b482c256db3a796c37d8ce3fee
Bruce Hill <bruce.w.hill@gmail.com>
2015-02-09 15:38:33 -0800

    Initial commit. Minimal working project.

---
+++ compress_office.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+
+# ----------------------------------------------------------------
+#  * Script to unzip, exify, and rezip a Microsoft Office XML file
+#  * Accepts 1 argument: a path to .docx, .xlsx, or .pptx file
+#  * Resulting compressed version is placed in same directory
+# ----------------------------------------------------------------
+
+
+# ------------------
+# EXI codec location
+# ------------------
+
+EXIFICIENT="java -jar ${PWD}/exip/exip.jar"
+
+# --------------------------------------
+# Verify file extension & file existence
+# Use regex to capture extension
+# --------------------------------------
+
+target=$1
+ext_regex='^(.*)\.(docx|xlsx|pptx)$'
+
+if [[ ${target} =~ ${ext_regex} ]]; then 
+	if [ -f ${target} ]; then
+
+		# -----------------------------------------------------
+		# Capture the file extension and convert for rezipping. 
+		# It is converted like so:
+		#   docx --> doce
+		#   xlsx --> xlse
+		#   pptx --> ppte
+		# -----------------------------------------------------
+		
+		path=${BASH_REMATCH[1]}
+		ext=${BASH_REMATCH[2]}
+
+		echo path:  ${path}
+		echo ext:   ${ext}
+
+
+		case ${ext} in
+        	docx)
+        	    ext='doce'
+        	    echo "Converting docx to ${ext}"
+        	    ;;
+	        xlsx)
+	            ext='xlse'
+	            echo "Converting xlsx to ${ext}"
+	            ;;
+	        pptx)
+	            ext='ppte'
+	            echo "Converting pptx to ${ext}"
+	            ;;
+	    esac
+
+
+		
+
+		# ---------------------
+		# Split up the filepath
+		# ---------------------
+
+		# #path=${f%/*.*}/
+  #   	base=$(basename ${f})
+  #   	ext=.${base##*.}
+  #   	name=${base%.*}
+
+  #   	echo base:  ${base}
+  #   	echo ext:   ${ext}
+  #   	echo name:  ${name}
+
+
+		# --------------------------------------
+		# Unzip the file into a temporary folder
+		# --------------------------------------
+		
+		mkdir temp
+		cp ${target} temp
+		cd temp
+		unzip $(basename ${target})
+		rm $(basename ${target})
+		 
+		# -------------------------------------------------
+		# Iterate through all XML files in temporary folder
+		# -------------------------------------------------
+
+		for f in `find . -type f -name "*.rels" -or -name "*.xml"`
+		do	
+
+			# ----------------------------------
+			# Compress with EXI in compress mode
+			# ----------------------------------
+
+			${EXIFICIENT} -xml_in ${f} -exi_out ${f}.exi -compression -preserve_prefixes
+			
+			# --------------------------
+			# Trash the uncompressed XML
+			# --------------------------
+		
+			rm ${f}
+
+		done
+
+		# ---------------------------------
+		# Rezip the file with new extension
+		# ---------------------------------
+		
+		zip -r ../${path}.${ext} *
+		cd ..
+
+		# ---------------------
+		# Trash the temp folder
+		# ---------------------
+
+		rm -r temp
+
+	fi
+fi
---
+++ decompress_office.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+
+# -------------------------------------------------------------
+#  * Script to restore an exified Microsoft Office XML file
+#  * Accepts 1 argument: a path to an .ooexi file
+#  * Resulting decompressed version is placed in same directory
+# -------------------------------------------------------------
+
+
+# ------------------
+# EXI codec location
+# ------------------
+
+EXIFICIENT="java -jar ${PWD}/exip/exip.jar"
+
+# --------------------------------------
+# Verify file extension & file existence
+# --------------------------------------
+
+target=$1
+ext_regex='^(.*)\.(doce|xlse|ppte)$'
+
+if [[ ${target} =~ ${ext_regex} ]]; then 
+	if [ -f ${target} ]; then
+
+		# -----------------------------------------------------
+		# Capture the file extension and convert for rezipping. 
+		# It is converted like so:
+		#   doce --> docx
+		#   xlse --> xlsx
+		#   ppte --> pptx
+		# -----------------------------------------------------
+		
+		path=${BASH_REMATCH[1]}
+		ext=${BASH_REMATCH[2]}
+
+		case ${ext} in
+        	doce)
+        	    ext='docx'
+        	    echo "Converting doce to ${ext}"
+        	    ;;
+	        xlse)
+	            ext='xlsx'
+	            echo "Converting xlse to ${ext}"
+	            ;;
+	        ppte)
+	            ext='pptx'
+	            echo "Converting ppte to ${ext}"
+	            ;;
+	    esac
+
+
+		# --------------------------------------
+		# Unzip the file into a temporary folder
+		# --------------------------------------
+		mkdir temp
+		cp ${target} temp
+		cd temp
+		unzip $(basename ${target})
+		rm $(basename ${target})
+
+		# -------------------------------------------------
+		# Iterate through all XML files in temporary folder
+		# -------------------------------------------------
+
+		for f in `find . -type f -name "*.exi"`
+		do	
+
+			# -------------------------
+			# Strip .exi file extension
+			# -------------------------
+
+			strip_ext_regex='^(.*)\.exi$'
+			[[ ${f} =~ ${strip_ext_regex} ]]
+			f_new=${BASH_REMATCH[1]}
+
+			# ---------------------------------------
+			# Decompress with EXI in compression mode
+			# ---------------------------------------
+
+			${EXIFICIENT} -exi_in ${f} -xml_out ${f_new} -compression -preserve_prefixes
+			
+			# -----------------------------
+			# Trash the compressed EXI file
+			# -----------------------------
+			rm ${f}
+
+		done
+
+		# --------------------------------------
+		# Rezip the file with original extension
+		# --------------------------------------
+
+		zip -r ../${path}.${ext} *
+		cd ..
+
+		# ---------------------
+		# Trash the temp folder
+		# ---------------------
+
+		rm -r temp
+
+	fi
+fi
